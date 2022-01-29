bcd503151da1926e2cf3539a5fb33112f086a4e8
Leon Colt <lionmanbloodsucker@yahoo.com>
2022-01-29 19:28:35 +0700

    refactor add function to refresh access token

--- lib/src/repository_auth_handler_mixin.dart
+++ lib/src/repository_auth_handler_mixin.dart
@@ -3,28 +3,49 @@ library get_connect_repo_mixin;
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/request/request.dart';
+import 'package:synchronized/synchronized.dart';
 
 mixin RepositoryAuthHandlerMixin<T> on GetConnect {
+  static final lock = Lock();
   @mustCallSuper
   @override
   void onInit() {
     super.onInit();
-    httpClient.addRequestModifier(
-      ( final Request request ) async {
-        request.headers['Authorization'] = await accessToken;
+    httpClient.addRequestModifier<T?>(
+      (request) async {
+        final accessToken = await lock.synchronized(() async {
+          if (shouldRefreshAccessToken) {
+            try {
+              return await refreshAccessToken();
+            } catch (error, st) {
+              onRefreshSessionError(error, st);
+              return null;
+            }
+          } else {
+            return await this.accessToken;
+          }
+        });
+        if (accessToken != null) {
+          request.headers['Authorization'] = accessToken;
+        }
         return request;
       },
     );
-    httpClient.addAuthenticator<dynamic>(
-      ( final Request request ) async {
-        request.headers['Authorization'] = await accessToken;
+    httpClient.addAuthenticator<T?>(
+      (request) async {
+        final accessToken =
+            await lock.synchronized(() async => await refreshAccessToken());
+        request.headers['Authorization'] = accessToken;
         return request;
       },
     );
   }
 
-  String getUrl( final Request<T> request ) => "${ request.url.scheme }://${ request.url.host }${ request.url.hasPort ? ":${ request.url.port }" : "" }";
+  String getUrl(final Request<T> request) =>
+      "${request.url.scheme}://${request.url.host}${request.url.hasPort ? ":${request.url.port}" : ""}";
 
   Future<String> get accessToken;
-
+  Future<String> refreshAccessToken();
+  bool get shouldRefreshAccessToken => false;
+  void onRefreshSessionError(dynamic error, StackTrace stackTrace) {}
 }
